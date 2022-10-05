4c04da98c6228b6a5ff8aec9c0bb4ccd5c32ce92
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2022-10-06 03:26:29 +0700

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -16,25 +16,25 @@ ANON = "https://telegra.ph/file/1e52aeabec63b21b2b534.jpg"
 async def repo(client, message):
     await message.reply_photo(
         photo=ANON,
-        caption=f"""**Hai​ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
+        caption=f"""**Hai​ {message.from_user.mention()},\n\nSaya [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
 
 **» Developer​ :** [DarkiezZzz](tg://user?id=1128130156)
-**» Python Version :** `{y()}`
-**» Library Version :** `{o}` 
-**» Telethon Version :** `{s}` 
-**» Pyrogram Version :** `{z}`
+**» Python versi :** `{y()}`
+**» Library versi :** `{o}` 
+**» Telethon versi :** `{s}` 
+**» Pyrogram versi :** `{z}`
 
-**Saya dibuat khusus untuk mengelolah grup, jika berminat hubungi owner saya.**
+**Saya dibuat khusus untuk mengelolah grup. jika berminat hubungi owner saya.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "• ᴏᴡɴᴇʀ •", url=f"https://t.me/{OWNER_USERNAME}"
+                        "• Owner •", url=f"https://t.me/{OWNER_USERNAME}"
                     ),
                     InlineKeyboardButton(
-                        "• sᴏᴜʀᴄᴇ •",
-                        url="https://bit.ly/Haotogel-OfficiaI",
+                        "• Source •",
+                        url="t.me/DarkiezZzz",
                     ),
                 ]
             ]
