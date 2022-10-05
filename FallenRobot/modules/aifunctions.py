22893ec029b8ecb7053931b209d84a4524e85866
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2022-10-05 13:05:14 +0700

    Update aifunctions.py

--- FallenRobot/modules/aifunctions.py
+++ FallenRobot/modules/aifunctions.py
@@ -28,7 +28,7 @@ async def hmm(event):
     lodu = response.json()
     result = lodu["message"]["text"]
     if "no no" in result:
-        pro = "I am fairly found and I was made by @anonymous_was_bot."
+        pro = "I am fairly found and I was made by @DarkiezZzz."
         try:
             async with telethn.action(event.chat_id, "typing"):
                 await asyncio.sleep(2)
@@ -36,7 +36,7 @@ async def hmm(event):
         except CFError as e:
             print(e)
     elif "ann" in result:
-        pro = "My name is Fallen"
+        pro = "My name is HAOTOGEL OFFICIAL"
         try:
             async with telethn.action(event.chat_id, "typing"):
                 await asyncio.sleep(2)
