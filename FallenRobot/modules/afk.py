b232c09ea4f600bdcde1921efd62af6b7becfd7e
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2022-10-05 14:47:15 +0700

    Update afk.py

--- FallenRobot/modules/afk.py
+++ FallenRobot/modules/afk.py
@@ -40,7 +40,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("{} Sekarang AFK!{}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -131,10 +131,10 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         if int(userc_id) == int(user_id):
             return
         if not user.reason:
-            res = "{} offine".format(fst_name)
+            res = "{} afk".format(fst_name)
             update.effective_message.reply_text(res)
         else:
-            res = "{} offine.\n<b>Alasan:</b> <code>{}</code>".format(
+            res = "{} afk.\n<b>Alasan:</b> <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
