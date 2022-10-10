f207dbf59f10ed94d07b318bc48d6b10bd55fe4f
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2022-10-11 02:31:07 +0700

    Update events.py

--- FallenRobot/events.py
+++ FallenRobot/events.py
@@ -82,7 +82,7 @@ def bot(**args):
     if pattern is not None and not pattern.startswith("(?i)"):
         args["pattern"] = "(?i)" + pattern
 
-    args["pattern"] = pattern.replace("#$", r_pattern, 1)
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
     stack = inspect.stack()
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
