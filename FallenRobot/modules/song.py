75c118ecbea468099fc6aa9a27d0bbfc8c389582
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2022-10-12 20:06:48 +0700

    Update song.py

--- FallenRobot/modules/song.py
+++ FallenRobot/modules/song.py
@@ -47,7 +47,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("» Mengunduh...\nMohon tunggu...")
+    m.edit("» Mengunduh...\n» Mohon tunggu...")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
