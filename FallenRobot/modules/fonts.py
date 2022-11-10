ef16672920b4970e1a7e3beda5d2db256e9ccf4b
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2022-11-10 23:23:59 +0700

    Update fonts.py

--- FallenRobot/modules/fonts.py
+++ FallenRobot/modules/fonts.py
@@ -1,8 +1,8 @@
-from pyrogram import Client, filters
+from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from FallenRobot.utils.fonts import Fonts
-
+from FallenRobot import pbot as Client
 
 @Client.on_message(filters.command(["font", "fonts"]))
 async def style_buttons(c, m, cb=False):
